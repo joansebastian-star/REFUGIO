3bf61d836c627fccdf3d22f2ee03e27f5186cfc1
Joan Yambay <joansebastian200720@gmail.com>
2026-09-21 23:36:10 -0500

    Creación Animal con atributos y constructor

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+
+    private int id;
+    private String nombre;
+    private int edad;
+    private String estado;
+
+    public Animal(int id, String nombre, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.estado = "DISPONIBLE";
+    }
+
+}
